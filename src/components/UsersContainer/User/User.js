768b29b35080d7f6485ca9e4978d2a2559bd5498
NadiiaKhynotska <nadinyman@gmail.com>
2023-07-08 23:09:06 +0300

    done homework 2 part 3

--- src/components/UsersContainer/User/User.js
+++ src/components/UsersContainer/User/User.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './User.module.css'
 
 const User = ({user,setUserId}) => {
     const {id,name,username,email} =user
+
+    // const [showPost, setShowPost] = useState(false);
 
     return (
         <div className={style.container}>
@@ -10,7 +12,7 @@ const User = ({user,setUserId}) => {
             <div>name: {name}</div>
             <div>username: {username}</div>
             <div>email: {email}</div>
-            <button onClick={()=>setUserId(id)}>Show posts</button>
+            <button onClick={()=>(setUserId(id))}> Show posts </button>
         </div>
     );
 };
