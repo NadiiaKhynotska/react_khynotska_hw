a0dbdd39b1f37c4e1cefc5f374428e4628999dc3
NadiiaKhynotska <nadinyman@gmail.com>
2023-07-12 01:48:15 +0300

    homework 3 part2 button logic

--- src/components/UsersComponent/UserComponent/UserComponent.js
+++ src/components/UsersComponent/UserComponent/UserComponent.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import {useState} from "react";
 
 import style from './UserComponent.module.css'
+
 const UserComponent = ({user, setUserId}) => {
+
+    const [showPost, setShowPost] = useState(false);
+    const [activeButton, setActiveButton] = useState(null)
+
+    const handelClick = () => {
+
+        setShowPost(!showPost)
+        showPost ? setUserId(null) : setUserId(user.id)
+
+        setActiveButton(!activeButton)
+        activeButton? setActiveButton(null): setActiveButton(user.id)
+
+        return ({
+            setActiveButton,
+            setUserId,
+            showPost
+        })
+    }
     return (
         <div className={style.container}>
             <div>User name: {user.name}</div>
             <div>User id: {user.id}</div>
-            <button onClick={()=>setUserId(user.id)} className={style.userButton}>Show posts of current user</button>
+            <button className={style.userButton}
+                    onClick={handelClick}
+                    disabled={activeButton !== user.id && activeButton !== null}>
+                {showPost ? 'Hide posts' : 'Show posts'} </button>
         </div>
-    );
-};
+    )
+}
 
 export default UserComponent;
