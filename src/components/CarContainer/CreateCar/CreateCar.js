b2aa4e9a8f3fb17c92477c3b4d141f221df1e758
NadiiaKhynotska <nadinyman@gmail.com>
2023-07-17 18:39:03 +0300

    homework 4 part2 correct update function, separated it from handleSubmit function

--- src/components/CarContainer/CreateCar/CreateCar.js
+++ src/components/CarContainer/CreateCar/CreateCar.js
@@ -7,14 +7,14 @@ const CreateCar = ({setOnSave, onUpdate, setOnUpdate}) => {
     let priceRef = useRef('')
     let yearRef = useRef('')
 
-    useEffect(()=>{
-        if(onUpdate){
+    useEffect(() => {
+        if (onUpdate) {
             brandRef.current.value = onUpdate.brand
             priceRef.current.value = onUpdate.price
             yearRef.current.value = onUpdate.year
         }
 
-    },[onUpdate])
+    }, [onUpdate])
     const handleSubmit = (e) => {
         e.preventDefault()
 
@@ -23,19 +23,6 @@ const CreateCar = ({setOnSave, onUpdate, setOnUpdate}) => {
             price: priceRef.current.value,
             year: yearRef.current.value,
         }
-        if(onUpdate){
-                fetch(`http://owu.linkpc.net/carsAPI/v1/cars/${onUpdate.id}`, {
-                    headers:{'content-type':'application/json'},
-                    method:'PUT',
-                    body:JSON.stringify(car)
-                }).then(value => value.json()).then(()=>{
-                    setOnSave(prev=>!prev)
-                    setOnUpdate(null)
-                    brandRef.current.value = ''
-                    priceRef.current.value = ''
-                    yearRef.current.value = ''
-                })
-        }else
         fetch('http://owu.linkpc.net/carsAPI/v1/cars', {
             method: 'POST',
             headers: {
@@ -45,7 +32,7 @@ const CreateCar = ({setOnSave, onUpdate, setOnUpdate}) => {
             body: JSON.stringify(car)
         })
             .then(response => response.json())
-            .then(car => {
+            .then(() => {
                 setOnSave(prev => !prev)
 
                 brandRef.current.value = ''
@@ -54,31 +41,58 @@ const CreateCar = ({setOnSave, onUpdate, setOnUpdate}) => {
             })
     }
 
-
+    const update = (e) => {
+        e.preventDefault()
+        if (onUpdate) {
+            const car = {
+                brand: brandRef.current.value,
+                price: priceRef.current.value,
+                year: yearRef.current.value,
+            }
+            fetch(`http://owu.linkpc.net/carsAPI/v1/cars/${onUpdate.id}`, {
+                headers: {'content-type': 'application/json'},
+                method: 'PUT',
+                body: JSON.stringify(car)
+            }).then(value => value.json()).then(() => {
+                setOnSave(prev => !prev)
+                setOnUpdate(null)
+                brandRef.current.value = ''
+                priceRef.current.value = ''
+                yearRef.current.value = ''
+            })
+        }
+    }
 
 
     return (
         <div className={style.create}>
-            <h2>{onUpdate? 'Update current car':'Create New Car'}</h2>
-            <form onSubmit={handleSubmit}>
+            <h2>{onUpdate ? 'Update current car' : 'Create New Car'}</h2>
+            <form onSubmit={onUpdate ? update : handleSubmit}>
                 <label>Brand</label>
                 <input
                     type='text'
                     ref={brandRef}
+                    required={true}
                 />
 
                 <label>Price</label>
                 <input
                     type='number'
                     ref={priceRef}
+                    min={0}
+                    max={1000000}
+                    required={true}
                 />
 
                 <label>Year of manufactured</label>
                 <input
                     type='number'
                     ref={yearRef}
+                    min={1990}
+                    max={new Date().getFullYear()}
+                    required={true}
                 />
-                <button>{onUpdate? 'Update car': 'Add new car'}</button>
+                <button disabled={!setOnSave}>{onUpdate ? 'Update car' : 'Add new car'}</button>
             </form>
         </div>
     );
