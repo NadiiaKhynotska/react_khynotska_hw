9f4656af589d5891b9ab2ebab5de1ba364196ac9
NadiiaKhynotska <nadinyman@gmail.com>
2023-08-17 16:18:54 +0300

    rick & morty redux toolkit

--- src/components/CharactersContainer/Characters/Characters.js
+++ src/components/CharactersContainer/Characters/Characters.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {useLocation} from "react-router-dom";
+import {characterActions} from "../../../redux/slices/charactersSlice";
 
 const Characters = () => {
+    const dispatch = useDispatch();
+    const {characters} = useSelector(state => state.characters)
+    const {state:{iDs}} = useLocation();
+    console.log(iDs)
+
+    useEffect(() => {
+        dispatch(characterActions.all(iDs))
+    }, []);
+    console.log(characters)
+
     return (
         <div>
             Characters
---
+++ src/redux/slices/charactersSlice.js
@@ -0,0 +1,41 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {characterService} from "../../services";
+
+
+const initialState = {
+    characters: []
+}
+
+ const all = createAsyncThunk(
+     'charactersSlice/all',
+     async ({iDs} ,thunkAPI)=>{
+         try {
+             const {data}= await (characterService.getAll(iDs))
+             return data
+         }catch (e) {
+             return thunkAPI.rejectWithValue(e.response.data)
+         }
+     }
+
+ )
+const charactersSlice = createSlice({
+        name: 'charactersSlice',
+        initialState,
+        reducers: {},
+        extraReducers: builder =>
+            builder.addCase(all.fulfilled, (state, action)=>{
+                state.characters = action.payload
+            })
+    }
+)
+
+const {reducer:characterReducer,actions}= charactersSlice
+
+const characterActions = {
+    all,
+}
+
+export {
+    characterReducer,
+    characterActions
+}
