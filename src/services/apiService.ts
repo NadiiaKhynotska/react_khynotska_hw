0ab41a78fc0b3374dc354ed0571ae307cf99b553
NadiiaKhynotska <nadinyman@gmail.com>
2023-08-25 17:09:19 +0300

    homework 15 interceptors

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,46 +1,69 @@
 import axios, {AxiosError, AxiosResponse} from "axios";
-import {baseURL} from "../constants";
+import {baseURL, urls} from "../constants";
 import {authService} from "./authService";
 import {rejects} from "assert";
+import {router} from "../router";
 
 type IRes<DATA> = Promise<AxiosResponse<DATA>>
 
 const apiService = axios.create({baseURL})
 
-apiService.interceptors.request.use( req =>{
+apiService.interceptors.request.use(req => {
     const access = authService.getAccessToken();
 
-    if(access){
+    if (access) {
         req.headers.Authorization = `Bearer ${access}`
     }
     return req
 })
 
 let isRefresh = false;
+const waitList:IWaitList[] = []
 apiService.interceptors.response.use(
-    res =>{
+    res => {
         return res
     },
-    async (error:AxiosError)=>{
+    async (error: AxiosError) => {
         const originalRequest = error.config
 
-        if(error.response.status === 401){
-            if(!isRefresh){
+        if (error.response.status === 401) {
+            if (!isRefresh) {
                 isRefresh = true
                 try {
                     await authService.refresh()
                     isRefresh = false
+                    afterRefresh()
                     return apiService(originalRequest)
-                }catch (e){
+                } catch (e) {
                     authService.deleteTokens()
                     isRefresh = false
+                    router.navigate('/login?SessionExpired=true')
                     return Promise.reject(error)
                 }
             }
-            return Promise.reject(error)
+            if(originalRequest.url === urls.auth.refresh){
+                return Promise.reject(error)
+            }
+            return new Promise(resolve => {
+                subscribeToWaitList(()=>resolve(apiService(originalRequest)))
+            })
         }
+        return Promise.reject(error)
     }
 )
+
+type IWaitList = () => void
+const subscribeToWaitList = (cb: IWaitList): void => {
+    waitList.push(cb)
+}
+
+const afterRefresh = (): void => {
+    while (waitList.length) {
+        const cb = waitList.pop();
+        cb()
+
+    }
+}
 export type {
     IRes
 }
